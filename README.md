0082ba862e007224da0cb8b4ba98e56cab5cba08
Michael <longtu12fall@gmail.com>
2015-12-15 14:22:07 -0800

    initial commit for influxdbUrl

---
+++ README.md
@@ -0,0 +1,59 @@
+# influxdbUrl
+
+This is a helper project to let frontend code connect to backend influxdb databse.
+By default, this service is bind to port 18080
+
+follow the instructions here to install go on your environment:
+https://golang.org/doc/install
+
+
+
+build all go file in influxdbUrl project
+```
+go install influxdbUrl/
+```
+
+If you have a username and password to encrypt.
+
+```
+go run influxdbUrl/encryptionGenerator.go ${YOUR_USERNAME}
+go run influxdbUrl/encryptionGenerator.go ${YOUR_PASSWORD}
+go run influxdbUrl/encryptionGenerator.go ${YOUR_URL_TO_DATABASE}
+go run influxdbUrl/encryptionGenerator.go ${YOUR_DATABASE_NAME}
+```
+This will generate a encryted version of your username, password, databseUrl and databaseName
+
+The database url should contain all sub directories. Some sample YOUR_URL_TO_DATABASE
+```
+http://localhost:8086/query
+http://paas.sym.com/db/k8s/series
+```
+
+Copy and paste the result generated from the above program to credential.config, in the following format:
+```
+u=${YOUR_ENCRYPTED_USERNAME}
+p=${YOUR_ENCRYPTED_PASSWORD}
+l=${YOUR_ENCRYPTED_URL}
+d=${YOUR_ENCRYPTED_DATABASE_NAME}
+```
+
+Use the following command to start the backend service
+```
+go run influxdbUrl/influxDBUrl2.go
+```
+
+Note: because this program use current working path for config file, it can only work correctly using the above relative path. Do not run it from a different path
+
+
+http syntax:
+```
+curl -H "Content-Type: application/json" -X POST -d "{\"timeStart\": \"2015-11-03\",\"timeEnd\": \"2015-11-04\",\"limit\": 1000,\"metric\": \"uptime_ms_cumulative\" }"  -i  http://localhost:18080/influxdbUrl
+```
+
+On the server side, it will print the logs of sql command constructed.
+On the client side, it will print out the http response, it there are any.
+
+After the above config, the last step is to change the file permmision of credential.config to read only
+```
+chmod 444 credential.config
+```
---
+++ influxDBUrl2.go
@@ -0,0 +1,236 @@
+package main
+
+import (
+	"bufio"
+	"crypto/aes"
+	"crypto/cipher"
+	"encoding/hex"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"strconv"
+	"strings"
+)
+
+const (
+	influxDbTag          = "/influxdbUrl"
+	portNum              = "18080"
+	defaultStartTime     = "1970-01-01 00:00:00.000"
+	timeLengthIndex  int = 19
+	timeStampSuffix      = ".000"
+)
+
+//Note: parameter name in this struct needs to start with upper case letter
+//PodID and Metric are required fields, the rest are optional
+type jsonStruct struct {
+	PodID     string
+	TimeStart string
+	TimeEnd   string
+	Limit     int
+	Metric    string
+}
+
+var commonIV = []byte{0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0a, 0x0b, 0x0c, 0x0d, 0x0e, 0x0f}
+
+func main() {
+	http.HandleFunc(influxDbTag, influxDBHandler)
+	log.Fatal(http.ListenAndServe(":"+portNum, nil))
+}
+
+func influxDBHandler(rw http.ResponseWriter, req *http.Request) {
+	decoder := json.NewDecoder(req.Body)
+	var t jsonStruct
+	err := decoder.Decode(&t)
+	if err != nil {
+		return
+	}
+
+	log.Println(t.PodID)
+	log.Println(t.TimeStart)
+	log.Println(t.TimeEnd)
+	log.Println(t.Limit)
+	log.Println(t.Metric)
+
+	podID := t.PodID
+	startTime := t.TimeStart
+	endTime := t.TimeEnd
+	limitNum := strconv.Itoa(t.Limit)
+	metrics := t.Metric
+
+	if len(metrics) <= 0 {
+		log.Println("no metrics")
+		return
+	}
+
+	if len(startTime) <= 0 {
+		startTime = defaultStartTime
+	}
+
+	sql := "SELECT * FROM \"" + metrics + "\" WHERE time > '" + startTime + "'"
+	if len(endTime) <= 0 {
+		sql += " AND time < now()"
+	}
+	if len(podID) > 0 {
+		sql += " AND PodID='" + podID + "'"
+	}
+
+	if len(limitNum) > 0 {
+		sql += " LIMIT " + limitNum
+	}
+
+	log.Println(sql)
+	res, err := readInfluxDb(sql, metrics)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	log.Println(string(res[:]))
+	a, err := json.Marshal(string(res[:]))
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	rw.Write(a)
+	return
+}
+
+func readInfluxDb(command string, metrics string) (res []byte, err error) {
+	credential, err := getCredentials()
+
+	if err != nil {
+		return res, err
+	}
+
+	//test username and pw
+	if len(credential[0]) <= 0 {
+		fmt.Println("no username")
+		return res, errors.New("no username")
+	}
+
+	if len(credential[1]) <= 0 {
+		fmt.Println("no password")
+		return res, errors.New("no password")
+	}
+
+	if len(credential[2]) <= 0 {
+		fmt.Println("no dbURL")
+		return res, errors.New("no dbURL")
+	}
+
+	if len(credential[3]) <= 0 {
+		fmt.Println("no dbName")
+		return res, errors.New("no dbName")
+	}
+	linkURL := credential[2] + "?u=" + credential[0] + "&p=" + credential[1] + "&pretty=true"
+	dbNameParam := "db=" + credential[3]
+	//command = "SELECT * FROM \"cpu/usage_ns_cumulative\" WHERE time > '1970-01-01 00:00:00' AND time <now() LIMIT 1"
+	SQLParam := "q=" + command
+	log.Println(linkURL)
+	log.Println(dbNameParam)
+	log.Println(SQLParam)
+	c, err := exec.Command("curl", "-G", linkURL, "--data-urlencode", dbNameParam, "--data-urlencode", SQLParam).Output()
+	if err != nil {
+		return res, err
+	}
+	res = c
+	return res, nil
+}
+
+func decypher(command string) (s string, err error) {
+	keyText := "astaxie12798akljzmknm.ahkjkljl;k"
+
+	// Create the aes encryption algorithm
+	c, err := aes.NewCipher([]byte(keyText))
+	if err != nil {
+		fmt.Printf("Error: NewCipher(%d bytes) = %s", len(keyText), err)
+		os.Exit(-1)
+	}
+
+	ciphertext2, _ := hex.DecodeString(command)
+
+	cfbdec := cipher.NewCFBDecrypter(c, commonIV)
+	plaintextCopy := make([]byte, len(ciphertext2))
+	cfbdec.XORKeyStream(plaintextCopy, ciphertext2)
+	fmt.Printf("%x=>%s\n", ciphertext2, plaintextCopy)
+	s = string(plaintextCopy[:])
+	return s, nil
+}
+
+func getCredentials() (credential []string, err error) {
+	credential = make([]string, 4)
+	absPath, _ := filepath.Abs("influxdbUrl/credential.config")
+	file, err := os.Open(absPath)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	username := ""
+	password := ""
+	dbURL := ""
+	dbName := ""
+	for scanner.Scan() {
+		line := scanner.Text()
+		if strings.Contains(line, "u=") {
+			username = line[2:]
+			continue
+		}
+		if strings.Contains(line, "p=") {
+			password = line[2:]
+			continue
+		}
+		if strings.Contains(line, "l=") {
+			dbURL = line[2:]
+			continue
+		}
+		if strings.Contains(line, "d=") {
+			dbName = line[2:]
+			continue
+		}
+	}
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
+
+	//test username and pw
+	if len(username) <= 0 {
+		fmt.Println("no username")
+		return credential, errors.New("no username")
+	}
+
+	realUsername, _ := decypher(username)
+	fmt.Println(realUsername)
+
+	if len(password) <= 0 {
+		fmt.Println("no password")
+		return credential, errors.New("no password")
+	}
+	realPassword, _ := decypher(password)
+	fmt.Println(realPassword)
+
+	if len(dbURL) <= 0 {
+		fmt.Println("no dbURL")
+		return credential, errors.New("no dbURL")
+	}
+	realURL, _ := decypher(dbURL)
+	fmt.Println(realURL)
+
+	if len(dbName) <= 0 {
+		fmt.Println("no dbName")
+		return credential, errors.New("no dbName")
+	}
+	realDBName, _ := decypher(dbName)
+	fmt.Println(realDBName)
+
+	credential[0] = realUsername
+	credential[1] = realPassword
+	credential[2] = realURL
+	credential[3] = realDBName
+	return credential, nil
+}
